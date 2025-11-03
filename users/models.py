233ee6c42c5b3b99d5f37d325ec0d99346020928
Lydia Christo <lydiachristo@lyles-macbook-pro.local>
2025-11-03 12:51:22 +0300

    Removed bio field from Profile model

--- users/models.py
+++ users/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(blank=True, null=True)
     profile_picture = models.ImageField(upload_to='profile_pics/', default='default.jpg')
 
     def __str__(self):
